8cd0eadd700b61f2ed48821ec1bdb22b9b853c00
nikithaa <nikithaa@PRINHYLTPAP1434.local>
2021-06-22 18:43:21 +0530

    user story 1 changes completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BbUIModule } from './components/bb-ui.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BbUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
